fb4a6215e0076d84e0364e630d3b44c78cb0115f
KimForest91 <forest_91@naver.com>
2023-03-16 14:37:55 +0900

    230316

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
+import Counter from './Counter';
+import Container from './Container';
+
+
 
 function App() {
+  let name = " 수빈수빈";
+  const number = 5;
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4, 
+    e: 5,
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div>
+        <MyHeader />
+        <h2>안녕안녕{name}</h2>
+        <p id="bold">React.js의 세계로 오신것을 환영합니다</p>
+        <p id="bold">{number}는 : {number % 2 === 0 ? "짝수" : "홀수"}</p>
+        <Counter {...counterProps} />
+        <MyFooter />
+      </div>
+    </Container>
   );
-}
+};
+
+
 
 export default App;
